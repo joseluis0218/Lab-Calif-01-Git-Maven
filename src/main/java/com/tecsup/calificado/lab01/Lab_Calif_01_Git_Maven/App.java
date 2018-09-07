e6812bf813c473b0d46e5ad47e6dd06638e926bd
Jose <jose.cristobal@tecsup.edu.pe>
2018-09-07 09:38:11 -0500

    Primer Commit

---
+++ src/main/java/com/tecsup/calificado/lab01/Lab_Calif_01_Git_Maven/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.calificado.lab01.Lab_Calif_01_Git_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
